229068cb119554f53677d662c912aec8f88c7a4e
Gerald Ramos <ramosgerald255@gmail.com>
2022-01-19 07:43:12 +0800

    initial commit: Installed Vue3, Tailwind, AnimXyz

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
+import VueAnimXyz from "@animxyz/vue3";
+import "@animxyz/core";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "./assets/sass/index.scss";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(VueAnimXyz).use(store).use(router).mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <XyzTransition appear xyz="fade-25% down flip-up">
+      <h1 class="h font-bold">Im Here</h1>
+    </XyzTransition>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
