d1502e53bdd202be2e883e5a7036f617d5bf44f2
Gerald Ramos <ramosgerald255@gmail.com>
2022-04-12 09:26:38 +0800

    fix: docs for gh pages

---
+++ docs/precache-manifest.69bafea12dd5d625ce1a5944a86469f2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50fc80bf18dca11bdd47",
+    "url": "/css/app.e8ca51ff.css"
+  },
+  {
+    "revision": "6efd0bd641fd1019892e",
+    "url": "/css/chunk-vendors.e9c65b69.css"
+  },
+  {
+    "revision": "07011464f11ba4c761a216e14031937b",
+    "url": "/img/TAU.07011464.png"
+  },
+  {
+    "revision": "cf1e7c6500ffc4eb7577a7c251a17a6c",
+    "url": "/img/andrea.cf1e7c65.jpg"
+  },
+  {
+    "revision": "2c43fe150bff3b40fdcfb9a7ba489874",
+    "url": "/img/bg.2c43fe15.jpg"
+  },
+  {
+    "revision": "244c0ac95256d8b3451b7fe8c3c431cf",
+    "url": "/img/justin.244c0ac9.jpg"
+  },
+  {
+    "revision": "c422c0c2de35aa55ccb501334c1224a9",
+    "url": "/img/pexel.c422c0c2.jpg"
+  },
+  {
+    "revision": "130034ee3a36c40e6ce5ba65667c823d",
+    "url": "/img/rice.130034ee.jpg"
+  },
+  {
+    "revision": "03f57fb7f42c91652f0dc12485b3e6df",
+    "url": "/img/rice2.03f57fb7.jpg"
+  },
+  {
+    "revision": "f020eb3723209ee555a151349e9434cb",
+    "url": "/img/rice3.f020eb37.jpg"
+  },
+  {
+    "revision": "f6aa6aafc7f71aa9727abb209fe9915b",
+    "url": "/img/rice5.f6aa6aaf.jpg"
+  },
+  {
+    "revision": "8c333584a9d9dd4b077a469caaf48183",
+    "url": "/img/robben.8c333584.jpg"
+  },
+  {
+    "revision": "d32b426656270a20e92e813cec3adf96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee0b495dcffeead1ee0c",
+    "url": "/js/about.4cac85b9.js"
+  },
+  {
+    "revision": "50fc80bf18dca11bdd47",
+    "url": "/js/app.9703880e.js"
+  },
+  {
+    "revision": "6efd0bd641fd1019892e",
+    "url": "/js/chunk-vendors.3b32ee01.js"
+  },
+  {
+    "revision": "92c481f3ee3a3507e71a9616a7ff91ac",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
